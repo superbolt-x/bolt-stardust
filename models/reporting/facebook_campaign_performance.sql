238c93b7e81866a244dc1fc47e340f69dc4c9b72
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-12-10 08:12:28 +0100

    Add app_install and custom event fields to SQL query

--- models/reporting/facebook_campaign_performance.sql
+++ models/reporting/facebook_campaign_performance.sql
@@ -12,6 +12,7 @@ date_granularity,
 spend,
 impressions,
 link_clicks,
-purchases,
-revenue
+omni_app_install as app_install,
+"app_custom_event.rc_trial_started_event" as trial_started,
+"app_custom_event.rc_initial_purchase_event" as initial_purchase
 FROM {{ ref('facebook_performance_by_campaign') }}
