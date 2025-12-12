6fca30a87995bf75caa47ffe1d8036f0135ad445
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2025-12-12 18:13:21 +0100

    Update blended.sql

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -10,6 +10,7 @@ WITH appsflyer_data AS (
         SELECT 
             '{{granularity}}' as date_granularity,
             date_trunc('{{granularity}}',date) as date,
+			app,
             CASE 
                 WHEN source = 'Facebook Ads' THEN 'Meta'
                 WHEN source = 'googleadwords_int' THEN 'Google Ads'
@@ -23,7 +24,7 @@ WITH appsflyer_data AS (
             sum(rc_trial_converted_users) as trial_converted,
             sum(rc_trial_started_users) as trial_started
         FROM {{ source('gsheet_raw','appsflyer_insights') }}
-        GROUP BY 1,2,3,4
+        GROUP BY 1,2,3,4,5
         {% if not loop.last %}UNION ALL{% endif %}
         {% endfor %}
     ),
@@ -33,22 +34,40 @@ paid_data as
         COALESCE(SUM(impressions),0) as impressions
     FROM
         (SELECT 'Meta' as channel, campaign_id, campaign_name, date, date_granularity, 
-            spend, link_clicks as clicks, impressions
+            case 
+				when campaign_name ~* '_ios_' then 'iOS'
+				when campaign_name ~* '_and_' then 'Android'
+				when campaign_name ~* '_web_' then 'Web'
+				else 'Other'
+			end as app,
+			spend, link_clicks as clicks, impressions
         FROM {{ source('reporting','facebook_ad_performance') }}
         UNION ALL
         SELECT 'Google Ads' as channel, campaign_id, campaign_name, date, date_granularity,
+			case 
+				when campaign_name ~* '_ios_' then 'iOS'
+				when campaign_name ~* '_and_' then 'Android'
+				when campaign_name ~* '_web_' then 'Web'
+				else 'Other'
+			end as app,
             spend, clicks, impressions
         FROM {{ source('reporting','googleads_campaign_performance') }}
 		UNION ALL
 		SELECT 'Tiktok Ads' as channel, campaign_id, campaign_name, date, date_granularity, 
+			case 
+				when campaign_name ~* '_ios_' then 'iOS'
+				when campaign_name ~* '_and_' then 'Android'
+				when campaign_name ~* '_web_' then 'Web'
+				else 'Other'
+			end as app,
 			spend, clicks, impressions
         FROM {{ source('reporting','tiktok_ad_performance') }}
         )
-    GROUP BY channel, campaign_id, campaign_name, date, date_granularity),
+    GROUP BY channel, campaign_id, campaign_name, date, date_granularity, app),
 
 paid_appsflyer_data as (
   SELECT 
-    channel, campaign_name, date::date, date_granularity,
+    channel, campaign_name, date::date, date_granularity, app,
     SUM(COALESCE(spend, 0)) AS spend,
     SUM(COALESCE(clicks, 0)) AS clicks,
     SUM(COALESCE(impressions, 0)) AS impressions,
@@ -57,11 +76,12 @@ paid_appsflyer_data as (
     SUM(COALESCE(initial_purchase, 0)) AS initial_purchase,
     SUM(COALESCE(trial_converted, 0)) AS trial_converted,
     SUM(COALESCE(trial_started, 0)) AS trial_started
-  FROM paid_data FULL OUTER JOIN appsflyer_data USING(channel,date,date_granularity,campaign_name)
+  FROM paid_data FULL OUTER JOIN appsflyer_data USING(channel,date,date_granularity,campaign_name,app)
   GROUP BY 1,2,3,4)
     
 SELECT 
     channel,
+	app,
     campaign_name,
     date,
     date_granularity,
