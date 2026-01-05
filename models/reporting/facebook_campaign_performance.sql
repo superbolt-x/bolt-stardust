fc9ee88c7dfe5a154c904b3a55beeae614f8f88a
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-01-05 10:34:38 +0100

    Update facebook_campaign_performance.sql

--- models/reporting/facebook_campaign_performance.sql
+++ models/reporting/facebook_campaign_performance.sql
@@ -14,5 +14,6 @@ impressions,
 link_clicks,
 omni_app_install as app_install,
 "app_custom_event.rc_trial_started_event" as trial_started,
+"app_custom_event.rc_trial_converted_event" as trial_converted,
 "app_custom_event.rc_initial_purchase_event" as initial_purchase
 FROM {{ ref('facebook_performance_by_campaign') }}
